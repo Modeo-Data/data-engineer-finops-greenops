8279ec4e18acc9364234631b5e8dae59bd1bf761
ArthyUthayarajah <150043317+ArthyUthayarajah@users.noreply.github.com>
2024-03-29 14:11:39 +0100

    Used a CTE for storage_usage_overview.sql

--- redshift/sql/storage_usage_overview.sql
+++ redshift/sql/storage_usage_overview.sql
@@ -1,9 +1,4 @@
-SELECT
-  schema,
-  table_id,
-  name AS table_name,
-  size_mb
-FROM (
+WITH storage_usage AS (
   SELECT
     tbl.schema,
     tbl.id AS table_id,
@@ -13,6 +8,12 @@ FROM (
   JOIN (SELECT DISTINCT id, schema, name FROM svv_table_info) tbl ON info.id = tbl.id
   WHERE info.slice = 0
   GROUP BY tbl.schema, tbl.id, tbl.name
-) AS storage_usage
+)
+SELECT
+  schema,
+  table_id,
+  name AS table_name,
+  size_mb
+FROM storage_usage
 ORDER BY size_mb DESC
 LIMIT 20;
